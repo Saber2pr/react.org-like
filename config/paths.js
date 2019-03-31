5249a62b1ed26771ed40c986591d2476823f8767
saber2pr <saber2pr@gmail.com>
2019-03-31 14:18:38 +0000

    update

--- config/paths.js
+++ config/paths.js
@@ -34,7 +34,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl = envPublicUrl ||
-    (publicUrl ? url.parse(publicUrl).pathname : '/build');
+    (publicUrl ? url.parse(publicUrl).pathname : '/react.org-like/build');
   return ensureSlash(servedUrl, true);
 }
 
